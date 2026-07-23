e642e09b839917b5e7e24c59827a74ef891b582f
anubhavkumar009 <mathuranubhav9@gmail.com>
2026-07-23 06:33:55 +0530

    Create server.js

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+require("dotenv").config();
+
+const express=require("express");
+const cors=require("cors");
+const connectDB=require("./config/db");
+const todoRoutes=require("./routes/todoRoutes");
+
+const app=express();
+
+app.use(express.json());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://your-vercel-app.vercel.app"
+    ]
+}));
+
+connectDB();
+
+app.use("/api/todos",todoRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
